f7bd0177165efb4491695a9196b66027ab63bf29
Lukáš Piják <lukaspijak@gmail.com>
2018-12-06 10:54:29 +0100

    new version

--- prestasms/src/init.php
+++ prestasms/src/init.php
@@ -11,7 +11,7 @@ define('_BG_PRESTASMS_AUTHOR_', 'TOPefekt s.r.o.');
 define('_BG_PRESTASMS_AUTHOR_URL_', 'https://www.bulkgate.com/');
 define('_BG_PRESTASMS_PS_MIN_VERSION_', '1.6.0.0');
 define('_BG_PRESTASMS_SLUG_', 'bg_prestasms');
-define('_BG_PRESTASMS_VERSION_', '4.5.0');
+define('_BG_PRESTASMS_VERSION_', '4.5.1');
 define('_BG_PRESTASMS_DEMO_', false);
 
 if(!file_exists(_BG_PRESTASMS_DIR_.'/extensions/src/_extension.php'))
