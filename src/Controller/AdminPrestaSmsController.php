730d3150e274755710d98eb936e5efe3860a9f6e
martin <martinkreizl@gmail.com>
2023-11-03 15:29:38 +0100

    debug action

--- src/Controller/AdminPrestaSmsController.php
+++ src/Controller/AdminPrestaSmsController.php
@@ -11,16 +11,30 @@ use Symfony\Component\HttpFoundation\Request;
  */
 class AdminPrestaSmsController extends FrameworkBundleAdminController
 {
-    public function __construct()
+    public function __construct(string $test)
     {
-
+        dump($test);
     }
 
     public function demoAction(Request $request)
     {
         dump($this, $request);
         return $this->render('@Modules/bg_prestasms/views/demo.html.twig', [
-            'layoutTitle' => 'BulkGate SMS'
+            'layoutTitle' => 'BulkGate SMS',
+        ]);
+    }
+
+    public function debugAction(Request $request)
+    {
+        /*$requirements = $requirements->run([
+            $requirements->same('{"message":"BulkGate API"}', file_get_contents('https://portal.bulkgate.com/api/welcome'), 'Api Connection'),
+            $requirements->same(true, version_compare(_PS_VERSION_, '1.7.5', '>='), 'WordPress ver. >= 1.7.5'),
+        ]);*/
+
+        return $this->render('@Modules/bg_prestasms/views/debug.html.twig', [
+            'layoutTitle' => 'BulkGate SMS - debug',
+            'php_version' => phpversion(),
+            'prestashop_version' => _PS_VERSION_,
         ]);
     }
 }
