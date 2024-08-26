075362bcc6be6930091933c6876d1d5601a18744
martin <martinkreizl@gmail.com>
2024-08-26 10:54:22 +0200

    debug data + fix SPA proxy redirect urls

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,6 +2,8 @@
 
 namespace BulkGate\PrestaSms\Controller;
 
+use BulkGate\Plugin\Debug\Logger;
+use BulkGate\Plugin\Debug\Requirements;
 use BulkGate\Plugin\IO\Url;
 use BulkGate\Plugin\Settings;
 use BulkGate\Plugin\Eshop;
@@ -36,17 +38,19 @@ class AdminController extends FrameworkBundleAdminController
         ]);
     }
 
-    public function debugAction(Request $request)
+    public function debugAction(Request $request, Requirements $requirements, Logger $logger)
     {
-        /*$requirements = $requirements->run([
+        $requirements = $requirements->run([
             $requirements->same('{"message":"BulkGate API"}', file_get_contents('https://portal.bulkgate.com/api/welcome'), 'Api Connection'),
-            $requirements->same(true, version_compare(_PS_VERSION_, '1.7.5', '>='), 'WordPress ver. >= 1.7.5'),
-        ]);*/
+            $requirements->same(true, version_compare(_PS_VERSION_, '1.7.5', '>='), 'Prestashop ver. >= 1.7.5'),
+        ]);
 
         return $this->render('@Modules/bg_prestasms/views/debug.html.twig', [
             'layoutTitle' => 'BulkGate SMS - debug',
             'php_version' => phpversion(),
             'prestashop_version' => _PS_VERSION_,
+			'requirements' => $requirements,
+			'errors' => array_reverse($logger->getList()),
         ]);
     }
 
@@ -57,11 +61,11 @@ class AdminController extends FrameworkBundleAdminController
             case "login":
                 ['email' => $email, 'password' => $password] = Json::decode($request->getContent());
 
-                return $this->json($sign->in($email, $password, $this->generateUrl('bulkgate_main_app', [], UrlGeneratorInterface::ABSOLUTE_URL) . '#/dashboard'));
+                return $this->json($sign->in($email, $password, '/dashboard'));
             case "logout":
-                return $this->json($sign->out($this->generateUrl('bulkgate_main_app', [], UrlGeneratorInterface::ABSOLUTE_URL) . '#/sign/in'));
+                return $this->json($sign->out('/sign/in'));
             case "authenticate":
-                return $this->json($authenticate->run($this->generateUrl('bulkgate_main_app', [], UrlGeneratorInterface::ABSOLUTE_URL) . '#/sign/in'));
+                return $this->json($authenticate->run('/sign/in'));
             case "module-settings":
                 $data = Json::decode($request->getContent());
 
