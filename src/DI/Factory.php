53d04962ff14894c40fb554b857870d498e67c5c
martin <martinkreizl@gmail.com>
2024-08-29 09:52:46 +0200

    implement Database\Connection - remove custom duplicated services

--- src/DI/Factory.php
+++ src/DI/Factory.php
@@ -16,7 +16,6 @@ use BulkGate\{
 	Plugin\DI\Factory as DIFactory,
 	};
 use BulkGate\PrestaSms\Database\Connection;
-use BulkGate\PrestaSms\Settings\Repository\{SettingsDatabase, SynchronizationDatabase};
 
 class Factory implements DIFactory
 {
@@ -31,63 +30,14 @@ class Factory implements DIFactory
 	{
 		$container = new Container($parameters['mode'] ?? 'strict');
 
-		/*$parameters['language'] ??= 'en';
-
-		// Ajax
-		$container['ajax.authenticate'] = Authenticate::class;
-		$container['ajax.plugin_settings'] = PluginSettingsChange::class;
-
-		// Debug
-		$container['debug.repository.logger'] = ['factory' => LoggerSettings::class, 'factory_method' => function () use ($container, $parameters): LoggerSettings
-		{
-			$service = new LoggerSettings($container->getByClass(Settings\Settings::class));
-			$service->setup(is_int($parameters['logger_limit'] ?? null) ? $parameters['logger_limit'] : 100);
-			return $service;
-		}];
-
-		// Eshop
-		$container['eshop.synchronizer'] = Eshop\EshopSynchronizer::class;
-		$container['eshop.configuration'] = ['factory' => Eshop\Configuration::class, 'factory_method' => fn () => new ConfigurationWordpress($parameters['plugin_data'] ?? [], $parameters['url'], $parameters['name'] ?? 'Store')];
-		$container['eshop.order_status'] = OrderStatusWordpress::class;
-		$container['eshop.return_status'] = ReturnStatusWordpress::class;
-        $container['eshop.language'] = LanguageWordpress::class;
-        $container['eshop.multistore'] = MultiStoreWordpress::class;
-
-        // Event loaders
-		$container['event.loader.extension'] = ['factory' => Extension::class, 'auto_wiring' => false];
-		$container['event.loader.shop'] = ['factory' => Shop::class, 'auto_wiring' => false];
-		$container['event.loader.order'] = ['factory' => Order::class, 'auto_wiring' => false];
-		$container['event.loader.order_status'] = ['factory' => OrderStatus::class, 'auto_wiring' => false];
-		$container['event.loader.customer'] = ['factory' => Customer::class, 'auto_wiring' => false];
-		$container['event.loader.product'] = ['factory' => Product::class, 'auto_wiring' => false];
-		$container['event.loader.post'] = ['factory' => Post::class, 'auto_wiring' => false];
-
-		// Event
-		$container['event.hook'] = ['factory' => Event\Hook::class, 'parameters' => ['version' => $parameters['api_version'] ?? '1.0']];
-		$container['event.asynchronous.repository'] = Event\Repository\AsynchronousDatabase::class;
-		$container['event.asynchronous'] = Event\Asynchronous::class;
-		$container['event.loader'] = ['factory' => Event\Loader::class, 'factory_method' => fn () => new Event\Loader([
-			$container->getByClass(Order::class),
-			$container->getByClass(OrderStatus::class),
-			$container->getByClass(Customer::class),
-			$container->getByClass(Shop::class),
-			$container->getByClass(Product::class),
-			$container->getByClass(Post::class),
-			$container->getByClass(Extension::class),
-		])];
-		$container['event.dispatcher'] = Event\Dispatcher::class;*/
-
         // Database
         $container['database.connection'] = ['factory' => Connection::class, 'parameters' => ['db' => $parameters['db']]];
 
 		// Settings
-		$container['settings.repository.database'] = SettingsDatabase::class;
+		$container['settings.repository.database'] = Settings\Repository\SettingsDatabase::class;
+		$container['settings.repository.synchronizer'] = Settings\Repository\SynchronizationDatabase::class;
 		$container['settings.settings'] = Settings\Settings::class;
-		$container['settings.repository.synchronizer'] = SynchronizationDatabase::class;
 		$container['settings.synchronizer'] = Settings\Synchronizer::class;
-
-		// User
-		//$container['user.sign'] = User\Sign::class;
 
 		return $container;
 	}
--- src/Database/Connection.php
+++ src/Database/Connection.php
@@ -10,11 +10,13 @@ namespace BulkGate\PrestaSms\Database;
 use BulkGate\Plugin\{Database\ResultCollection, Strict, Database};
 use Doctrine\DBAL;
 
-class Connection //implements Database\Connection
+class Connection implements Database\Connection
 {
 	use Strict;
 
 	private $db;
+
+	private array $prepare_parameters = [];
 
 	/**
 	 * @var list<string>
@@ -27,13 +29,14 @@ class Connection //implements Database\Connection
 	}
 
 
-	public function execute(string $sql, array $parameters = []): ?ResultCollection
+	public function execute(string $sql): ?ResultCollection
 	{
 		$output = new ResultCollection();
 
 		$this->sql[] = $sql;
 
-		$result = $this->db->executeQuery($sql, $parameters)->fetchAllAssociative();
+		$result = $this->db->executeQuery($sql, $this->prepare_parameters)->fetchAllAssociative();
+		$this->prepare_parameters = [];
 
         foreach ($result as $key => $item)
         {
@@ -64,5 +67,18 @@ class Connection //implements Database\Connection
 	public function table(string $table): string
 	{
 		return $this->prefix() . $table;
+	}
+
+	public function prepare(string $sql, ...$parameters): string
+	{
+		$this->prepare_parameters = $parameters;
+
+		// plugin's SQL queries are using %s for placeholder values, but Doctrine uses "?" character as placeholder
+		return str_replace("%s", "?", $sql);
+	}
+
+	public function escape(string $string): string
+	{
+		return $string;
 	}
 }
