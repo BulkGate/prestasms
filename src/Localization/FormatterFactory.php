4a61a066ab0f1dabd95bcc0ae8dae4c3b90869cd
martin <martinkreizl@gmail.com>
2024-08-29 07:07:13 +0200

    organize services

---
+++ src/Localization/FormatterFactory.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace BulkGate\PrestaSms\Localization;
+
+use BulkGate\Plugin\Localization\Formatter;
+use BulkGate\Plugin\Localization\FormatterBasic;
+use BulkGate\Plugin\Localization\FormatterIntl;
+
+class FormatterFactory
+{
+	public static function create(string $language): Formatter
+	{
+		if (extension_loaded("intl"))
+		{
+			return new FormatterIntl($language);
+		}
+		else
+		{
+			return new FormatterBasic();
+		}
+	}
+}
