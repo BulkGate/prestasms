16af8c7a21669552b631d122466085d97747cb3e
martin <martinkreizl@gmail.com>
2025-06-06 07:43:38 +0200

    add distribution.php

---
+++ distribution.php
@@ -0,0 +1,20 @@
+<?php
+
+/** WhiteLabel re-branding */
+
+// Your module's whitelabel name.
+const BulkGateWhiteLabel = 'BulkGate';
+
+const BulkGateWhiteLabelUrl = 'https://portal.bulkgate.com';
+
+// Your module's product page.
+const BulkGateWhiteLabelModuleWebsite = 'https://www.bulkgate.com/en/integrations/prestasms-sms-module-for-prestashop/';
+
+/** Affiliate program */
+
+// Your BulkGate's Affilitate ID. This can be found in your BulkGate account > Affiliate program.
+const BulkGateAffiliateId = '';
+
+/** Internals */
+
+const BulkGateModuleVersion = '6.0.0';
