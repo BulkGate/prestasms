21469cfc392cc7f4ba76fdd412cf98cd766869f4
martin <martinkreizl@gmail.com>
2025-06-09 15:53:33 +0200

    php-cs-fixer - respect .gitignore files

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -2,10 +2,19 @@
 
 $config = new PrestaShop\CodingStandards\CsFixer\Config();
 
+// get git's ignored files and format for cs-fixer's finder usage
+$ignored = [];
+exec('git ls-files --others --ignored --exclude-standard | cut -d/ -f1 | sort -u', $ignored);
+
 $finder = PhpCsFixer\Finder::create();
 $finder
 	->in(__DIR__)
 	->exclude('vendor');
+
+foreach ($ignored as $pathOrFile)
+{
+	$finder->exclude($pathOrFile);
+}
 
 return $config
 	->setUsingCache(true)
