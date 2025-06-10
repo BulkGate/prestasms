45f74705459e263e3abad71639fd280b739a6191
martin <martinkreizl@gmail.com>
2025-06-10 12:22:30 +0200

    finalize docker image build script logic

--- .docker/generate.sh
+++ .docker/generate.sh
@@ -1,15 +1,26 @@
 set -e
 
-# Load versions from JSON and emit --build-arg parameters
-build_args_from_versions() {
-    local versions_file="$1"
+save_version_into_file() {
+    local versions_file="$SCRIPT_DIR/versions.json"
+    local php_version="$1"
+    local prestashop_version="$2"
+
     php -r '
-        $versions = json_decode(file_get_contents($argv[1]), true);
-        if (!$versions) { exit(1); }
-        foreach ($versions as $k => $val) {
-            printf("--build-arg %s=%s ", strtoupper($k) . "_VERSION", $val);
-        }
-    ' "$versions_file"
+        $file = $argv[1];
+        $php = $argv[2];
+        $ps = $argv[3];
+        $arr = json_decode(file_get_contents($file), true);
+        $arr[] = ["php" => $php, "prestashop" => $ps];
+        // Odstraň duplicity
+        $arr = array_map("unserialize", array_unique(array_map("serialize", $arr)));
+        // Seřaď podle prestashop, pak php
+        usort($arr, function($a, $b) {
+            $cmp = version_compare($a["prestashop"], $b["prestashop"]);
+            if ($cmp !== 0) return $cmp;
+            return version_compare($a["php"], $b["php"]);
+        });
+        file_put_contents($file, json_encode($arr, JSON_PRETTY_PRINT|JSON_UNESCAPED_SLASHES));
+    ' "$versions_file" "$php_version" "$prestashop_version"
 }
 
 # get directory of this file
@@ -17,6 +28,10 @@ SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
 
 # Zpracování argumentů
 PUSH=1
+EMIT_VERSION=1
+PHP_VERSION=""
+PRESTASHOP_VERSION=""
+BUILD_ARGS=""
 
 while [[ $# -gt 0 ]]; do
     case "$1" in
@@ -24,18 +39,45 @@ while [[ $# -gt 0 ]]; do
             PUSH=0
             shift
             ;;
+        --no-emit-version)
+            EMIT_VERSION=0
+            shift
+            ;;
+        --php-version)
+            PHP_VERSION="$2"
+            shift 2
+            ;;
+        --prestashop-version)
+            PRESTASHOP_VERSION="$2"
+            shift 2
+            ;;
         *)
             shift
             ;;
     esac
 done
 
+
+if [[ -n "$PHP_VERSION" && -n "$PRESTASHOP_VERSION" ]]; then
+    BUILD_ARGS="$BUILD_ARGS --build-arg PHP_VERSION=$PHP_VERSION --build-arg PRESTASHOP_VERSION=$PRESTASHOP_VERSION"
+fi
+
+echo "Build args: $BUILD_ARGS"
+
+exit
+
 # build image
-docker build $(build_args_from_versions "$SCRIPT_DIR/versions.json") "$SCRIPT_DIR" -t ghcr.io/bulkgate/prestasms
+docker build $BUILD_ARGS "$SCRIPT_DIR" -t ghcr.io/bulkgate/prestasms
 VERSION=$(docker inspect --format '{{ .Config.Labels.version }}' ghcr.io/bulkgate/prestasms)
 
 # tag image
 docker image tag ghcr.io/bulkgate/prestasms ghcr.io/bulkgate/prestasms:$VERSION
+
+# add version matrix
+if [[ -n "$BUILD_ARGS" && $EMIT_VERSION -eq 1 ]]; then
+    echo "writing version into file ... $VERSION"
+    save_version_into_file $PHP_VERSION $PRESTASHOP_VERSION
+fi
 
 # push image
 if [ $PUSH -eq 1 ]; then
--- .docker/readme.md
+++ .docker/readme.md
@@ -2,28 +2,27 @@
 Tento image je určen pro účely vývoje prestaSMS modulu. Image můžete použít k napojení do IDE a také ke spuštění webové aplikace.
 Image obsahuje instalaci composeru a xdebugu. [Seznam dostupných images](https://github.com/BulkGate/prestasms/pkgs/container/prestasms).
 
-## 1. Konfigurace
-Nastavit můžete verzi php a prestashopu.
+## 1. Definování matice verzí
+V souboru versions.json jsou uloženy meta informace o matici dostupných verzí php a prestashop aplikace. Pokud tento seznam potřebujete rozšířit, tak stačí spustit:
 
-```
-.docker/versions.json
+```shell
+.docker/generate.sh --php-version 8.4 --prestashop-version 9.0.0 --no-push
 ```
 
 ## 2. Vytvoření image
-Spusťte shell script.
+Pokud chcete pro lokální účely vytvořit image, který nechcete distribuovat do registru a ani přidat do versions.json, můžete spustit:
+
 
 ```shell
-.docker/generate.sh # vyrvori image a pushne do registru
-.docker/generate.sh --no-push # vytvori image, nebude image pushovat do registru
+.docker/generate.sh --php-version=8.4 --prestashop-version=9.0.0 --no-push --no-emit-version
 ```
 
-> Každé spuštění scriptu vygeneruje 2 image a to (ghcr.io/bulkgate/prestasms) pro lokální použití a (ghcr.io/bulkgate/prestasms:8.2.0-8.1) pro nahrání do registru
+> Každé spuštění scriptu vygeneruje 2 image a to (ghcr.io/bulkgate/prestasms) pro lokální použití a (ghcr.io/bulkgate/prestasms:9.0.0-8.4) pro nahrání do registru
 
 
 ## 3. Použití image
 
-
 ```yaml
 prestashop:
-    image: ghcr.io/bulkgate/prestasms:8.2.0-8.1
+    image: ghcr.io/bulkgate/prestasms:8.2.1-8.1
 ```
